2ae3ac773deff2328894f88e70baa1e1778f36e4
Shishir Jha <shishirjhaccr@gmail.com>
2020-02-20 01:55:09 +0530

    simplest express app created

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express'); //importing express
+
+//using express. Single app may contain multiple express apps but usually only single express object is used including this app.
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+app.listen(5000); //listen to the port 5000
